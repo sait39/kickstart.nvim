1e5235bbd423bb1561ad160d34be570c9265fd9c
Sait <sait108@proton.me>
2024-06-01 11:15:53 +0200

    feat: add ts and tailwindcss servers in conf

--- init.lua
+++ init.lua
@@ -489,7 +489,8 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
-        -- tsserver = {},
+        tsserver = {},
+        tailwindcss = {},
         --
 
         lua_ls = {
